7bd90ccbb4803d0c5e61d4ec405bd495c3b2dfc9
ivopavlov <pavlov.ivo@gmail.com>
2019-09-15 21:15:15 -0700

    Frontend kind of works

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
-
+const User = require('./models/User');
+const passport = require('passport');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+  
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World, I have arrived! With Mongoose connected DB!"));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+
+// app.get("/", (req, res) => res.send("Hello World, I have arrived! With Mongoose connected DB!"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
